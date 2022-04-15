0ff24d951e46cfd254b7835d2e781b14a330a7e3
Haider Ali <haider.ali@navico.com>
2022-04-15 16:16:12 +0200

    init repository

---
+++ src/components/LoginItem.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="box">
+  <div id="header">
+    <div id="cont-lock"><i class="material-icons lock">lock</i></div>
+    <div id="bottom-head"><h1 id="logintoregister">Login</h1></div>
+  </div> 
+   <form action="" method="post">
+    <div class="group">      
+      <input class="inputMaterial" type="text" required>
+      <span class="highlight"></span>
+      <span class="bar"></span>
+      <label>Username</label>
+    </div>
+	    <div class="group">      
+      <input class="inputMaterial" type="password" required>
+      <span class="highlight"></span>
+      <span class="bar"></span>
+      <label>Password</label>
+    </div>
+    <button id="buttonlogintoregister" type="submit">Login</button>
+  </form>
+  <div id="footer-box"><p class="footer-text">Not a member?<span class="sign-up"> Sign up now</span></p></div>
+</div>
+
+</template>
+
+
+<style scoped lang="scss">
+    @import './LoginItem.scss';    
+</style>>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LoginItem from '@/components/LoginItem.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <LoginItem />
+    
+  </main>
+</template>
